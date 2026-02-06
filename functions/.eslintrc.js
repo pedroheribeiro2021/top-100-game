a520fbcd01910308855cd6e1c32addaed6c641ff
PEDRO HENRIQUE SILVA RIBEIRO <pedro.ribeiro@centralit.com.br>
2026-02-06 16:16:06 -0300

    feat: bootstrap fastify app and firebase entrypoint

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,33 +1,14 @@
 module.exports = {
   root: true,
   env: {
-    es6: true,
+    es2021: true,
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
-    sourceType: "module",
-  },
-  ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
-    "/generated/**/*", // Ignore generated files.
-  ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   rules: {
-    "quotes": ["error", "double"],
-    "import/no-unresolved": 0,
-    "indent": ["error", 2],
+    '@typescript-eslint/no-unused-vars': 'warn',
+    quotes: ['error', 'single'],
   },
 };
