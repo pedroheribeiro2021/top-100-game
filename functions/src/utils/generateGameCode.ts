7b43a18bee34e5805ace391adabff5896dc8412b
PEDRO HENRIQUE SILVA RIBEIRO <pedro.ribeiro@centralit.com.br>
2026-02-19 15:56:48 -0300

    feat: add game code generator

---
+++ functions/src/utils/generateGameCode.ts
@@ -0,0 +1,10 @@
+export function generateGameCode(): string {
+  const chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
+  let code = '';
+
+  for (let i = 0; i < 6; i++) {
+    code += chars[Math.floor(Math.random() * chars.length)];
+  }
+
+  return code;
+}
