d1f78f7cc66f9ba8dfbf5edfc277e050e3158598
PEDRO HENRIQUE SILVA RIBEIRO <pedro.ribeiro@centralit.com.br>
2026-02-25 23:05:30 -0300

    feat(game): implement join game endpoint with status validation

--- functions/src/routes/games.routes.ts
+++ functions/src/routes/games.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
-import { createGameHandler, getGameHandler } from '../controllers/games.controller'
+import { createGameHandler, getGameHandler, joinGameHandler } from '../controllers/games.controller'
 
 export const gamesRoutes = Router()
 
 gamesRoutes.post('/', createGameHandler)
 gamesRoutes.get('/:id', getGameHandler)
+gamesRoutes.post('/join', joinGameHandler)  
